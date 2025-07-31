9f445cfa6dd01aaed5e1da456914b024dc067d03
Omar Faruq <omarfaruq9723@gmail.com>
2025-07-31 16:20:28 +0600

    es6 js

--- react_learn/src/App.jsx
+++ react_learn/src/App.jsx
@@ -1,15 +1,29 @@
-import Login, {Profile, Image, Setting, UserKey, Add} from './UserComponent'
-function App(){
-  return(
+function App() {
+  
+  //moder syntex &  ES6 Arrow Function
+  const testClick = () => {
+    alert("Function work")
+  }
+
+  // function testClick() {
+  //   alert("function call");
+  // }
+  return (
     <div>
-      <h1>Import and Export Components as User Profile</h1>
-      <Login/>      
-      <Profile/>
-      <Image/>
-      <Setting/>
-      <h1>{UserKey}</h1>
-      <Add/>      
+      <h1>Omar Faruq React js Learning</h1>
+      <img
+        src="https://images.tractorjunction.com/5_Bajaj_Pulsar_N160_f00e3a2003.png?format=webp&quality=40"
+        height={350}
+        width={550}
+      />
+
+      <ul>
+        <li>Name: Omar Faruq</li>
+        <li>Designation: Software Engineer</li>
+        <li>Hobbies: Football and time spend with family</li>
+      </ul>
+      <button onClick={testClick}>Click me</button>
     </div>
-  )
+  );
 }
 export default App;
