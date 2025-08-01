d53f0801abedfa9e1d5d42517bef15e91b159172
Omar Faruq <omarfaruq9723@gmail.com>
2025-08-01 12:40:25 +0600

    jump to page initial

--- online-kids-book/src/App.jsx
+++ online-kids-book/src/App.jsx
@@ -4,15 +4,26 @@ import { sculptureList } from "./info";
 export default function Book() {
   const [index, setIndex] = useState(0);
   const [showMore, setShowMore] = useState(false);
+  let sculpture = sculptureList[index];
 
   function handleNextClick() {
-    setIndex(index + 1);
+    setIndex((prevIndex) => (prevIndex +1  % sculptureList.length));
     setShowMore(false);
   }
-  function handleShowMore() {
-    setShowMore(!showMore);
+
+  function handleToggle(){
+    setShowMore((prev) => !prev);
   }
-  let sculpture = sculptureList[index];
+
+  function handleJumpToPage(pageIndex){
+    setIndex(pageIndex);
+    setShowMore(false);
+  }
+
+//   function handleShowMore() {
+//     setShowMore(!showMore);
+//   }
+  
 
   return (
     <div style={{ textAlign: "center" }}>
@@ -23,7 +34,7 @@ export default function Book() {
       <img src={sculpture.imgUrl}  width="300" height="250"/>
 
       <div>
-        <button onClick={handleShowMore}>
+        <button onClick={handleToggle}>
         {showMore ? "Hide" : "Show"} details
       </button>
       {showMore && (
@@ -55,6 +66,26 @@ export default function Book() {
       <h3>
         ({index + 1} page of {sculptureList.length})
       </h3>
+
+      <div style={{ marginBottom: "15px" }}>
+        {sculptureList.map((_, i) => (
+          <button
+            key={i}
+            onClick={() => handleJumpToPage(i)}
+            style={{
+              margin: "2px",
+              padding: "5px 10px",
+              backgroundColor: index === i ? "#4CAF50" : "#e0e0e0",
+              color: index === i ? "white" : "black",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+          >
+            {i + 1}
+          </button>
+        ))}
+      </div>
 
       <button onClick={handleNextClick}>Next Page</button>
     </div>
