9d185f3b63355f09274f85cdf41d74f64e06a3f7
Omar Faruq <omarfaruq9723@gmail.com>
2025-08-02 18:51:17 +0600

    route

---
+++ drinks-bar/src/pages/home.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="banner-container">
+        <div className="banner">
+          <p>
+            Welcome to our Drinks Bar! Discover refreshing beverages and enjoy a
+            cozy atmosphere. Book your table and time easily to relax and savor
+            your favorite drinks with friends. Cheers to great moments and new
+            favorites!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
