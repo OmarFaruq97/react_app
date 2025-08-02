bdb6cfdbc6b9f470923e7563999e7270036fb758
Omar Faruq <omarfaruq9723@gmail.com>
2025-08-02 21:06:28 +0600

    init

---
+++ drinks-bar/src/components/NoMatch.jsx
@@ -0,0 +1,12 @@
+import "./NoMatch.css";
+const NoMatch = () => {
+  return (
+    <div className="container">
+      <div className="error-page">
+        <h1 className="error-code">404</h1>
+        <p className="error-text"> Page Not found</p>
+      </div>
+    </div>
+  );
+};
+export default NoMatch;
