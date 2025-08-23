eec6a5c98f539831e098e14c3464ca8be412bd90
Omar Faruq <omarfaruq9723@gmail.com>
2025-08-24 05:01:33 +0600

    Update ProductDetail.jsx

--- drinks-bar/drinks-bar/src/pages/ProductDetail.jsx
+++ drinks-bar/drinks-bar/src/pages/ProductDetail.jsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-// import "./ProductDetail.css"; // <-- external CSS
 import "./css/ProductDetail.css";
 
 const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const ProductDetail = () => {
-  const { id } = useParams();
   const [product, setProduct] = useState(null);
+  const { id } = useParams();
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -15,26 +14,30 @@ const ProductDetail = () => {
       const data = await response.json();
       setProduct(data.drinks[0]);
     };
-
     fetchProduct();
   }, [id]);
-
   if (!product) {
-    return <div>Loading...</div>;
+    return <div>Loading...please wait</div>;
   }
-
   return (
     <div className="product-detail-container">
       <img src={product.strDrinkThumb} alt={product.strDrink} />
       <div className="product-detail-info">
         <h1>{product.strDrink}</h1>
-        <p><strong>Category:</strong> {product.strCategory}</p>
-        <p><strong>Alcoholic:</strong> {product.strAlcoholic}</p>
-        <p><strong>Glass:</strong> {product.strGlass}</p>
-        <p><strong>Instructions:</strong> {product.strInstructions}</p>
+        <p>
+          <strong>Category:</strong> {product.strCategory}
+        </p>
+        <p>
+          <strong>Alcoholic:</strong> {product.strAlcoholic}
+        </p>
+        <p>
+          <strong>Glass:</strong> {product.strGlass}
+        </p>
+        <p>
+          <strong>Instructions:</strong> {product.strInstructions}
+        </p>
       </div>
     </div>
   );
 };
-
 export default ProductDetail;
